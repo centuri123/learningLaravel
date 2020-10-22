17dfccd3592391a9032f5bd72802a400cea1b60b
Carlos Eduardo <dudu.braz.leite@hotmail.com>
2020-10-22 11:45:11 -0300

    CRUD em Laravel via GET

---
+++ app-laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductModel;
+use Illuminate\Http\Request;
+
+use function PHPSTORM_META\type;
+
+class ProductController extends Controller
+{
+    private $productModel;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductModel;
+    }
+
+    public function index()
+    {
+        return $this->productModel->getAll();
+    }
+
+    public function showOne($productId)
+    {
+        $product = $this->productModel->getOne($productId);
+        if($product->isEmpty())
+        {
+            return 'Produto não encontrado!';
+        }
+        return $product;
+    }
+
+    public function insert($name, $quantity, $value)
+    {
+        $product = [
+            'name'     => $name,
+            'quantity' => $quantity,
+            'value'    => $value
+        ];
+        $this->productModel->insert($product);
+        return 'Produto inserido com sucesso!';
+    }
+
+    public function delete($productId)
+    {
+        $this->productModel->deleteProduct($productId);
+        return 'O registro escolhido foi deletado com sucesso!';
+    }
+
+    public function alter($productId, $name, $quantity, $value)
+    {
+        $oldProduct = $this->productModel->getOne($productId);
+        $altProduct = [
+            'name'     => $name,
+            'quantity' => $quantity,
+            'value'    => $value 
+        ];
+        $this->productModel->alter($productId, $altProduct);
+        $altProduct = $this->productModel->getOne($productId);
+
+        $returnText = "Antes - Nome:{$oldProduct[0]->name} Quantidade: {$oldProduct[0]->quantity} Valor: {$oldProduct[0]->value}<br>
+                       Depois - Nome:{$altProduct[0]->name} Quantidade: {$altProduct[0]->quantity} Valor: {$altProduct[0]->value}";
+        return $returnText;
+    }
+}
--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -2,15 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group([
+    'prefix' => 'products',
+], function(){
+    Route::get('/', 'ProductController@index')->name('index');
+    Route::get('/{productId}', 'ProductController@showOne')->name('showOne');
+    Route::get('/insert/{name}/{quantity}/{value}', 'ProductController@insert')->name('insert');
+    Route::get('/alter/{productId}/{name}/{quantity}/{value}', 'ProductController@alter')->name('alter');
+    Route::get('/delete/{productId}', 'ProductController@delete')->name('delete');
+}
+);
+
+
+/**
+    Referência didática sobre route
+*/
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+Route::get('/login', function(){
+    return "Login de autenticação!";
+})->name('login');
+
+
+Route::group([
+    'middleware' => ['auth'],
+    'prefix'     => 'admin',
+    'namespace'  => 'Admin',
+    'name'       => 'admin.' 
+], function(){
+        Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+
+        Route::get('/product', 'TesteController@teste')->name('product');
+
+        Route::get('/', function(){
+            return redirect()->route('dashboard');
+        })->name('home');
+});
 */
 
 Route::get('/', function () {
